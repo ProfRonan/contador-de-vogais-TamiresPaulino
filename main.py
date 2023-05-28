8e0df1b93436d7b820aa53a7e503105b4db03c40
TamireaPaulino <tamirespaulino19@alu.ufc.br>
2023-05-28 13:47:47 -0300

    atividade

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 """This program counts the number of vowels in a string."""
-
+from main import count_vowels
 def count_vowels(string:str) -> int:
+    vogais = "aeiou"
+    totalVogal = 0
+        for letra in string:
+            if letra in vogais:
+                totalVogal += 1
+    return totalVogal
     """
     Takes in a string and returns the number of vowels in the string.
 
